97dbc93601b002b53a71a9df4de9b671c1f7e410
salah <amsaafisalah2@gmail.com>
2024-02-29 13:31:15 +0100

    creating files

---
+++ client.c
@@ -0,0 +1,6 @@
+#include "mini_talk.h"
+
+int main(int ac, int *av[])
+{
+    
+}
---
+++ mini_talk.h
@@ -0,0 +1,10 @@
+#include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#ifndef MINI_TALK_H
+#define MINI_TALK_H
+
+
+
+#endif
