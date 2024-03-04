fd461aee24236b83e01e7c4eca419d84f42274fa
samsaafi <samsaafi@student.1337.ma>
2024-03-04 14:23:40 +0100

    client 40% done

--- client.c
+++ client.c
@@ -1,22 +1,62 @@
-#include <signal.h>
-#include <stdlib.h>
-#include <unistd.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: samsaafi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/04 12:07:37 by samsaafi          #+#    #+#             */
+/*   Updated: 2024/03/04 13:06:07 by samsaafi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-void handler(int sigtype)
+#include "mini_talk.h"
+
+static int glb = 0;
+
+void    sig_handler(int sigint, siginfo_t *info, void *context)
 {
-    write(1, "HANDLER EXEC\n", 13);
+    static  int i;
+    
+    i = 0;
+    glb = 1;
+    if (sigint == SIGUSR2)
+        i++;
+    else if (sigint == SIGUSR1)
+        write(1, "recieved bits\n", 14);
+}
+
+int char_to_bin(char c, int pid)
+{
+    int x;
+    int bit;
+
+    bit = 8;
+    while (bit > 0)
+    {
+        x = 0;
+        
+        if ((c >> bit) & 1)
+            kill(pid, SIGUSR1);
+        else
+            kill(pid, SIGUSR2);
+        while (glb == 0)
+        {
+            if (x = 42)
+                write(1, "TIMEOUT: no response from server\n", 33);
+            x++;
+            usleep(70);
+        }
+        glb = 0;
+        bit--;
+    }
+    return (0);
 }
 
 int main(int ac, int *av[])
 {
-    struct sigaction sg_action;
+    struct sigaction   sa;
+    char    *msg;
+    int pid;
 
-    sg_action.sa_handler = handler;
-    sigemptyset(&sg_action.sa_mask);
-    sg_action.sa_flags = SA_RESTART;
-    sigaction(SIGINT, &sg_action, NULL);
-    while(1)
-    {
-        sleep(1);
-    }
 }
